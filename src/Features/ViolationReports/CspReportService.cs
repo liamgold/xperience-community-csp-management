3a877ba67f5302dfe208ee7ccfdd88c38335f0b8
Liam Goldfinch <liam.goldfinch@idhl.co.uk>
2024-10-20 22:12:35 +0100

    feat(report): log violation reports to the database

--- src/Features/ViolationReports/CspReportService.cs
+++ src/Features/ViolationReports/CspReportService.cs
@@ -1,4 +1,6 @@
-﻿namespace XperienceCommunity.CSP.Features.ViolationReports
+﻿using CMS.DataEngine;
+
+namespace XperienceCommunity.CSP.Features.ViolationReports
 {
     public interface ICspReportService
     {
@@ -9,16 +11,44 @@
 
     public class CspReportService : ICspReportService
     {
+        private readonly IInfoProvider<CSPViolationReportInfo> _cspViolationReportInfoProvider;
+
+        public CspReportService(IInfoProvider<CSPViolationReportInfo> cspViolationReportInfoProvider)
+        {
+            _cspViolationReportInfoProvider = cspViolationReportInfoProvider;
+        }
+
         public async Task<bool> ProcessViolationReport(CspViolationReport violationReport)
         {
-            // TODO: add logic to process a single violation report
+            var violationReportInfo = new CSPViolationReportInfo
+            {
+                Age = violationReport.Age,
+                Type = violationReport.Type,
+                Url = violationReport.Url,
+                UserAgent = violationReport.UserAgent,
+                BlockedURL = violationReport.Body?.BlockedURL,
+                Disposition = violationReport.Body?.Disposition,
+                DocumentURL = violationReport.Body?.DocumentURL,
+                EffectiveDirective = violationReport.Body?.EffectiveDirective,
+                LineNumber = violationReport.Body?.LineNumber ?? 0,
+                OriginalPolicy = violationReport.Body?.OriginalPolicy,
+                Referrer = violationReport.Body?.Referrer,
+                Sample = violationReport.Body?.Sample,
+                SourceFile = violationReport.Body?.SourceFile,
+                StatusCode = violationReport.Body?.StatusCode ?? 0,
+                ReportedAt = DateTime.UtcNow,
+            };
+
+            _cspViolationReportInfoProvider.Set(violationReportInfo);
 
             return true;
         }
 
         public async Task<bool> ProcessViolationReports(IReadOnlyCollection<CspViolationReport> violationReports)
         {
-            // TODO: add logic to process multiple violation reports
+            var tasks = violationReports.Select(ProcessViolationReport);
+
+            await Task.WhenAll(tasks);
 
             return true;
         }
